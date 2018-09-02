ff7979596acfd974d58405d97ef8250e5b27158c
Thiago Bonfim <bonfimtm@gmail.com>
2018-09-02 20:05:27 +0200

    Thymeleaf and resources

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -2,15 +2,13 @@ package com.teamtreehouse.giflib.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GifController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String listGifs() {
-        return "Welcome my son!";
+        return "home";
     }
 
 }
