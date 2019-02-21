bc00c2b801d80cf4665d8f18a7a14686f4dc313d
Thiago Bonfim <bonfimtm@gmail.com>
2019-02-21 22:30:32 +0100

    Create Category entity, repository and controller

---
+++ src/main/java/com/teamtreehouse/giflib/controller/CategoryController.java
@@ -0,0 +1,32 @@
+package com.teamtreehouse.giflib.controller;
+
+import com.teamtreehouse.giflib.data.CategoryRepository;
+import com.teamtreehouse.giflib.data.GifRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private GifRepository gifRepository;
+
+    @RequestMapping("/categories")
+    public String listCategories(ModelMap modelMap) {
+        modelMap.put("categories", categoryRepository.findAll());
+        return "categories";
+    }
+
+    @RequestMapping("/category/{id}")
+    public String categoryDetail(@PathVariable int id, ModelMap modelMap) {
+        modelMap.put("category", categoryRepository.findById(id));
+        modelMap.put("gifs", gifRepository.findByCategoryId(id));
+        return "category";
+    }
+}
--- src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
+++ src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
@@ -6,17 +6,18 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class GifRepository {
 
     private static final List<Gif> ALL_GIFS = Arrays.asList(
-            new Gif("android-explosion", LocalDate.of(2015, 2, 13), "Chris Ramacciotti", false),
-            new Gif("ben-and-mike", LocalDate.of(2015, 10, 30), "Ben Jakuben", true),
-            new Gif("book-dominos", LocalDate.of(2015, 9, 15), "Craig Dennis", false),
-            new Gif("compiler-bot", LocalDate.of(2015, 2, 13), "Ada Lovelace", true),
-            new Gif("cowboy-coder", LocalDate.of(2015, 2, 13), "Grace Hopper", false),
-            new Gif("infinite-andrew", LocalDate.of(2015, 8, 23), "Marissa Mayer", true)
+            new Gif("android-explosion", LocalDate.of(2015, 2, 13), "Chris Ramacciotti", false, 1),
+            new Gif("ben-and-mike", LocalDate.of(2015, 10, 30), "Ben Jakuben", true, 0),
+            new Gif("book-dominos", LocalDate.of(2015, 9, 15), "Craig Dennis", false, 0),
+            new Gif("compiler-bot", LocalDate.of(2015, 2, 13), "Ada Lovelace", true, 2),
+            new Gif("cowboy-coder", LocalDate.of(2015, 2, 13), "Grace Hopper", false, 0),
+            new Gif("infinite-andrew", LocalDate.of(2015, 8, 23), "Marissa Mayer", true, 1)
     );
 
     public List<Gif> getAllGifs() {
@@ -24,12 +25,18 @@ public class GifRepository {
     }
 
     public Gif findByName(String name) {
-        for (Gif gif: ALL_GIFS) {
+        for (Gif gif : ALL_GIFS) {
             if (gif.getName().equals(name)) {
                 return gif;
             }
         }
         return null;
+    }
+
+    public List<Gif> findByCategoryId(int categoryId) {
+        return ALL_GIFS.stream()
+                .filter(gif -> categoryId == gif.getCategoryId())
+                .collect(Collectors.toList());
     }
 
 }
