c37d25db4860f3b5a3754726816e7ab51ce30718
Thiago Bonfim <bonfimtm@gmail.com>
2019-03-30 09:36:25 +0100

    Update GIFs categories

--- src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
+++ src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
@@ -12,12 +12,12 @@ import java.util.stream.Collectors;
 public class GifRepository {
 
     private static final List<Gif> ALL_GIFS = Arrays.asList(
-            new Gif("android-explosion", LocalDate.of(2015, 2, 13), "Chris Ramacciotti", false, 1),
-            new Gif("ben-and-mike", LocalDate.of(2015, 10, 30), "Ben Jakuben", true, 0),
-            new Gif("book-dominos", LocalDate.of(2015, 9, 15), "Craig Dennis", false, 0),
-            new Gif("compiler-bot", LocalDate.of(2015, 2, 13), "Ada Lovelace", true, 2),
-            new Gif("cowboy-coder", LocalDate.of(2015, 2, 13), "Grace Hopper", false, 0),
-            new Gif("infinite-andrew", LocalDate.of(2015, 8, 23), "Marissa Mayer", true, 1)
+            new Gif("android-explosion", LocalDate.of(2015, 2, 13), "Chris Ramacciotti", false, 3),
+            new Gif("ben-and-mike", LocalDate.of(2015, 10, 30), "Ben Jakuben", true, 2),
+            new Gif("book-dominos", LocalDate.of(2015, 9, 15), "Craig Dennis", false, 1),
+            new Gif("compiler-bot", LocalDate.of(2015, 2, 13), "Ada Lovelace", true, 1),
+            new Gif("cowboy-coder", LocalDate.of(2015, 2, 13), "Grace Hopper", false, 2),
+            new Gif("infinite-andrew", LocalDate.of(2015, 8, 23), "Marissa Mayer", true, 2)
     );
 
     public List<Gif> getAllGifs() {
